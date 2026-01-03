db612d74aaa7a4f7a721226366e63514948a3174
Fabio Benevenuti <fbenevenuti@inf.ufrgs.br>
2026-01-03 16:52:00 -0300

    Minor fixes on file naming.

--- build-osal-noel-v-freertos.sh
+++ build-osal-noel-v-freertos.sh
@@ -5,8 +5,8 @@ THIS_SCRIPT_FULLNAME=$(realpath "$THIS_SCRIPT")
 THIS_SCRIPT=$(basename "${THIS_SCRIPT_FULLNAME}")
 ROOT_DIR=$(dirname "$THIS_SCRIPT_FULLNAME")
 
-PATH_TOOLCHAIN_FILE="../tests_defs/toolchain-riscv-noel-v-freertos.cmake"
-BUILD_DIR="build_osal-riscv-noel-v-freertos"
+PATH_TOOLCHAIN_FILE="../tests_defs/toolchain-riscv-noelv-freertos.cmake"
+BUILD_DIR="build_osal-riscv-noelv-freertos"
 
 cd ${ROOT_DIR}
 
